00c31f67a79e887632d1f7a44bfe5a892fc96bda
Khalid <mess-34@hotmail.fr>
2021-05-06 10:15:22 +0200

    second commit

--- parse_log copy.py
+++ parse_log copy.py
@@ -2,7 +2,11 @@ f = open("planning.log", "r", encoding="utf-8")
 
 texte = f.read()
 
-for mot in texte:
-    
+def minute():
 
-print(texte)
+    ligne = texte.readlines
+    newline = ligne.split()
+
+    return newline
+
+    print(newline)
