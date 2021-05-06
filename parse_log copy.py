5e32120b5247e53b0327d7e91970b77f54b4faf8
Khalid <mess-34@hotmail.fr>
2021-05-06 09:45:15 +0200

    premier_commit

---
+++ parse_log copy.py
@@ -0,0 +1,8 @@
+f = open("planning.log", "r", encoding="utf-8")
+
+texte = f.read()
+
+for mot in texte:
+    
+
+print(texte)
---
+++ parse_log.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_minute(self):
+        self.assertEqual('09:20-11:00 Introduction', '100')
+
+
+if __name__ == '__main__':
+    unittest.main()
