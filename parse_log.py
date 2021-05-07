fc4ac0b6c27837892a8b0b83bb77d38a2249239a
Khalid <mess-34@hotmail.fr>
2021-05-07 16:31:05 +0200

    version2

--- parse_log.py
+++ parse_log.py
@@ -1,9 +1,12 @@
 import unittest
+from parse_log_copy import minute
 
 
 class TestStringMethods(unittest.TestCase):
+
     def test_minute(self):
-        self.assertEqual('09:20-11:00 Introduction', '100')
+        self.assertEqual(minute(221,222), 0:01:00)
+        self.assertNotAlmostEqual(minute(221,222), 0:02:00)
 
 
 if __name__ == '__main__':
