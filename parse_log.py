3d329db49911c778e92f00452bfe8260da49f055
Khalid <mess-34@hotmail.fr>
2021-05-10 13:50:30 +0200

    parsed

--- parse_log.py
+++ parse_log.py
@@ -1,13 +1,70 @@
-import unittest
-from parse_log_copy import minute
+import datetime
+import logging
 
 
-class TestStringMethods(unittest.TestCase):
+def lecture(fichier):
+    # fonction qui permet de ouvrire et de lire un fichier texte
+    f = open(fichier, "r", encoding="utf-8")
+    logging.info("ouverture du fichier")
+    texte = f.read()
+    logging.info("lecture du fichier")
+    # texte = texte.strip("\n")
+    return texte
+    logging.info("retourne le fichier")
+    
+    
 
-    def test_minute(self):
-        self.assertEqual(minute(221,222), 0:01:00)
-        self.assertNotAlmostEqual(minute(221,222), 0:02:00)
+def minute(min, min2):
+    
+    # fonction qui permet de soustraire des minute 
+    a = datetime.timedelta(minutes=min)
+    b = datetime.timedelta(minutes=min2)
+
+    return b - a
+    logging.info("calcule la soustraction en minute")
+
+def relever_valeur():
+    l = lecture('planning.log')
+    l = l.split()
+    for i in l:
+        return i
+
+    # for i in l:
+    #     # i = i.strip(",")
+    #     # list_temps[i] = 
+    # return l
 
 
-if __name__ == '__main__':
-    unittest.main()
+
+print(relever_valeur())
+
+
+# print(minute(221,222))
+
+# print(lecture('planning.log'))
+
+
+
+# f = open("planning.log", "r", encoding="utf-8")
+
+#     texte = f.read()
+#     texte = texte.strip("\n")
+
+
+# time = ['09:20', '11:00']
+
+# print(type(time))
+
+
+# print(time int([0]-[1]))
+
+# a = datetime.timedelta(hours=9, minutes=20)
+# b = datetime.timedelta(hours=11, minutes=0)
+
+# print(b-a)
+
+
+    # return newline
+
+    # print(newline)
+
