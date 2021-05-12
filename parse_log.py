a01a362233a56e23fad64378c3e07e28b30a0bd2
Khalid <mess-34@hotmail.fr>
2021-05-12 15:02:05 +0200

    parse

--- parse_log.py
+++ parse_log.py
@@ -14,33 +14,43 @@ def lecture(fichier):
     
     
 
-def minute(min, min2):
+# def minute(min, min2):
     
-    # fonction qui permet de soustraire des minute 
-    a = datetime.timedelta(minutes=min)
-    b = datetime.timedelta(minutes=min2)
+#     # fonction qui permet de soustraire des minute 
+#     a = datetime.timedelta(minutes=min)
+#     b = datetime.timedelta(minutes=min2)
 
-    return b - a
-    logging.info("calcule la soustraction en minute")
+    # return b - a
+    # logging.info("calcule la soustraction en minute")
 
 def converter(heure):
+    # fonction qui permet de soustraire des minute 
     heure1 = int(heure[0:2])*60 + int(heure[3:5])
+    logging.info("releve la premier valeur en heure est la convertie en minute")
     heure2 = int(heure[6:8])*60 + int(heure[9:11])
+    logging.info("releve la deuxième valeur en heure est la convertie en minute")
     return heure2 - heure1
+    logging.info(f"fais de la soustraction des minute de {heure2} et {heure1}")
 
 def relever_valeur():
     # l = lecture('planning.log')
     f = open('planning.log', "r", encoding="utf-8")
+    logging.info("ouverture du fichier")
     # l = l[0:10]
     # for i in f.readlines():
     f =  f.readlines()
     valeur_min = []
     for i in f:
         if i != '\n':
-            valeur_min.append(converter(i[0:11]))
+            valeur_min.append(i[12:-1])
+            valeur_min.append(converter((i[0:11])))
+            logging.info("releve et convertir en minute")
     # valeur_min ="".join(valeur_min)        
-    return valeur_min 
+    return valeur_min
+    
             # print(converter(i[0:11]))
+
+# print(relever_valeur())
 
 def theme():
     f = open('planning.log',"r", encoding="utf-8")
@@ -49,6 +59,7 @@ def theme():
     for i in texte:
         if i != '\n':
             nom_valeur.append(i[12:-1])
+            logging.info("releve les theme")
             # print(i[12:-1])
     return nom_valeur
     
@@ -56,21 +67,44 @@ def regroupement(dict1, dict2):
     dict_from_list = {}
     for t in dict1:
         for r in dict2:
-            dict_from_list[t] = r
-            dict2.remove(r)
-            break
+            if t in dict_from_list:
+                dict_from_list[t] += r
+            else:
+                dict_from_list[t] = r
+    logging.info("retourne le regroupement sous forme de dict les donne de chaque tache ")
     return dict_from_list
+    
 
     # dict_from_list = dict(zip(dict1, dict2))
     # return dict_from_list 
 
+def test_dict(dicts):
+    res = {}
+    for i in dicts:
+        key = i.split()[0]
+        value = i.split()[1]
+        print(key)
+        print(value)
+        if key in res:
+            res[key]+= int(value)
+        else:
+            res[key]=int(value)
+    return res
 
 
+# print(theme())
 
-print(theme())
 print(relever_valeur())
-print(regroupement(theme(),relever_valeur() ))
+# print(regroupement(theme(),relever_valeur()))
+# test_dictdf=["Introduction 40", "Break 30", "Introduction 20"]
+# res = {}
+# for i in test_dict:
+#     value = i.split()[0]
+#     minu = i.split()[1]
+#     print(minu)
+#     print(value)
 
+# print(test_dict(relever_valeur()))
 # print(converter('09:20-11:00')) 
     
 
@@ -78,35 +112,11 @@ print(regroupement(theme(),relever_valeur() ))
 
 
 
-# print(relever_valeur())
-
-
-# print(minute(221,222))
-
-# print(lecture('planning.log'))
 
 
 
-# f = open("planning.log", "r", encoding="utf-8")
-
-#     texte = f.read()
-#     texte = texte.strip("\n")
 
 
-# time = ['09:20', '11:00']
-
-# print(type(time))
 
 
-# print(time int([0]-[1]))
-
-# a = datetime.timedelta(hours=9, minutes=20)
-# b = datetime.timedelta(hours=11, minutes=0)
-
-# print(b-a)
-
-
-    # return newline
-
-    # print(newline)
 
--- parse_log_test.py
+++ parse_log_test.py
@@ -1,5 +1,5 @@
 import unittest
-from parse_log_copy import minute, lecture
+from parse_log_copy import minute, lecture, relever_valeur
 
 
 class TestStringMethods(unittest.TestCase):
@@ -10,6 +10,9 @@ class TestStringMethods(unittest.TestCase):
 
     def test_lecture(self):
         self.assertIsInstance(lecture('planning.log'), str)
+
+    def test_relever_valeur(self):
+        self.assertEqual(relever_valeur(),[100, 15, 20, 55, 60, 40, 20, 10, 60, 80, 30, 60, 20, 70, 30, 15, 90, 105, 15, 15, 30, 30])
 
 if __name__ == '__main__':
     unittest.main()
